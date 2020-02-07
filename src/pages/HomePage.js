55571d7277bf737e0987e3daa4bf2a181e849e2a
Lucas Teixeira <lucas.teixeira@calindra.com.br>
2020-02-07 20:17:05 -0300

    Criadas páginas básicas

---
+++ src/pages/HomePage.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import HeaderComponent from '../components/HeaderComponent'
+import CarouselComponent from '../components/CarouselComponent'
+import VideoComponent from '../components/VideoComponent'
+import SectionComponent from '../components/SectionComponent'
+import FooterComponent from '../components/FooterComponent'
+
+import '../App.css'
+
+export default class HomePage extends React.Component {
+    styles = {
+        div: {
+            color: '#fff',
+            backgroundColor: '#000',
+            width: '100%',
+            height: window.outerHeight
+        }
+    }
+    render() {
+        return (
+            <div className="App" style={this.styles.div}>
+                <HeaderComponent pages={['Nossa história', 'Fotos', 'Localização']} />
+                <CarouselComponent src={'https://picsum.photos/200'} />
+                {/* <VideoComponent /> */}
+                <SectionComponent>
+                    <h1>S1</h1>
+                </SectionComponent>
+                <SectionComponent>
+                    <h1>S1</h1>
+                </SectionComponent>
+                <SectionComponent>
+                    <h1>S1</h1>
+                </SectionComponent>
+                <FooterComponent />
+            </div>
+        )
+    }
+}
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+export default class PageNotFound extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>NOSSA HISTÓRIA</h1>
+            </div>
+        )
+    }
+}
