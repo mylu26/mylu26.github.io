9232e26a4abfcf7653f8402ecc3cd773e95aad1d
Lucas Teixeira <lucas.teixeira@calindra.com.br>
2020-02-08 16:50:45 -0300

    Updates

---
+++ precache-manifest.5d11fe18eb2ad2bacab151b75353f034.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd817941bef452961347bcf88bbddd5b",
+    "url": "/site-casamento/index.html"
+  },
+  {
+    "revision": "8e824f794772641f65a5",
+    "url": "/site-casamento/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d5ede50df196553d5149",
+    "url": "/site-casamento/static/css/main.b4046080.chunk.css"
+  },
+  {
+    "revision": "8e824f794772641f65a5",
+    "url": "/site-casamento/static/js/2.85c5c6a1.chunk.js"
+  },
+  {
+    "revision": "5f2dfc729cbc5e03eb64677a2da4dad4",
+    "url": "/site-casamento/static/js/2.85c5c6a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ede50df196553d5149",
+    "url": "/site-casamento/static/js/main.fc7cf279.chunk.js"
+  },
+  {
+    "revision": "9a01cf3675ee34cc69a5",
+    "url": "/site-casamento/static/js/runtime-main.716de1d8.js"
+  },
+  {
+    "revision": "186b9749c0922bc8eb6311d8187f2895",
+    "url": "/site-casamento/static/media/foto1.186b9749.jpg"
+  },
+  {
+    "revision": "1630066d986edef87994d61f7052b774",
+    "url": "/site-casamento/static/media/foto2.1630066d.jpg"
+  },
+  {
+    "revision": "b2866ecf787512ddaf76daef7ab9d1bd",
+    "url": "/site-casamento/static/media/foto3.b2866ecf.jpg"
+  },
+  {
+    "revision": "357e9eb12d67915eb322f987e0d86c4b",
+    "url": "/site-casamento/static/media/foto4.357e9eb1.jpg"
+  },
+  {
+    "revision": "9f94bd78fc419cb7c141915fed938aba",
+    "url": "/site-casamento/static/media/galeria1.9f94bd78.jpg"
+  },
+  {
+    "revision": "afa7e69813232799abf7809995d38ec3",
+    "url": "/site-casamento/static/media/mylenalucas_web.afa7e698.webm"
+  }
+]);
