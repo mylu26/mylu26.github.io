0e54e1b394a185966c07ab9b1c5b7d5f2966d1d5
Lucas Teixeira <lucas.teixeira@calindra.com.br>
2020-02-08 14:58:55 -0300

    Ajuste

--- src/components/SectionComponent.js
+++ src/components/SectionComponent.js
@@ -3,16 +3,6 @@ import { Navbar } from 'react-bootstrap'
 
 export default class SectionComponent extends React.Component {
     render() {
-        return (
-            <Navbar fixed={'bottom'}>
-                <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text>
-                        Signed in as: <a href="#login">Mark Otto</a>
-                    </Navbar.Text>
-                </Navbar.Collapse>
-            </Navbar>
-        )
+        return <div></div>
     }
 }
