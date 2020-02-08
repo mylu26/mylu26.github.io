a67961c0cdd8dface50958873f1a107619f65031
Lucas Teixeira <lucas.teixeira@calindra.com.br>
2020-02-08 11:19:14 -0300

    Ajuste nas rotas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import HomePage from '../src/pages/HomePage'
 import HistoryPage from '../src/pages/HistoryPage'
+import CountdownPage from '../src/pages/CountdownPage'
+import GaleryPage from '../src/pages/GaleryPage'
 import PageNotFound from '../src/pages/PageNotFound'
 import './App.css'
 
@@ -12,6 +14,11 @@ export default class App extends React.Component {
                 <Switch>
                     <Route path="/" exact={true} component={HomePage} />
                     <Route path="/history" exact={true} component={HistoryPage} />
+                    <Route path="/photos" exact={true} component={GaleryPage} />
+                    {/* <Route path="/giftList" exact={true} component={GiftPage} /> */}
+                    {/* <Route path="/sendMessage" exact={true} component={MessagePage} /> */}
+                    <Route path="/countdown" exact={true} component={CountdownPage} />
+                    {/* <Route path="/address" exact={true} component={AddressPage} /> */}
                     <Route path="*" component={PageNotFound} />
                 </Switch>
             </BrowserRouter>
